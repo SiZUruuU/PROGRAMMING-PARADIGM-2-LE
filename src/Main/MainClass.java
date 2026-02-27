31aa60a4aa9712bf8c18ca03eab53768a9ee86c0
SiZUruuU <savagefeeble@gmail.com>
2026-02-27 21:25:33 +0800

    created window using JFrame

--- src/Main/MainClass.java
+++ src/Main/MainClass.java
@@ -1,4 +1,16 @@
 package Main;
 
+import javax.swing.JFrame;
+
 public class MainClass {
+    public static void main(String args[]){
+        
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("Recipe++");
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+    }
 }
